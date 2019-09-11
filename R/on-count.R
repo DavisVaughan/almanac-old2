412bb872d990edc994b12f3521821b0a5a133bc9
DavisVaughan <davis@rstudio.com>
2019-09-11 00:08:33 -0400

    `on_count()`

---
+++ R/on-count.R
@@ -0,0 +1,46 @@
+#' @export
+on_count <- function(event, n, within = "month") {
+  assert_event(event)
+
+  n <- vec_cast(n, integer())
+  vec_assert(n, size = 1L)
+
+  arg_match(within, c("day", "week", "month", "quarter", "year"))
+
+  test <- function(env) {
+    x <- current_date(env)
+
+    split <- vctrs::vec_split_id(floor_date(x, within))
+    ids <- split$id
+
+    period_start <- split$key
+    period_end <- unique(ceiling_date(x, within)) - days(1L)
+
+    n_unique <- length(ids)
+
+    out <- vctrs::vec_init(logical(), vec_size(x))
+
+    for (i in seq_len(n_unique)) {
+      id <- ids[[i]]
+      range <- seq(period_start[i], period_end[i], "day")
+      result <- event_is(range, event)
+      result <- which(result)
+
+      if (length(result) < n) {
+        out[id] <- FALSE
+        next
+      }
+
+      loc <- result[[n]]
+
+      out[id] <- x[id] == range[loc]
+    }
+
+    out
+  }
+
+  new_event(
+    description = "On count",
+    test = test
+  )
+}
