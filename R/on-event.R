3d9dfc2fcee53dc6a6a3e488f676c8d053ae69b9
DavisVaughan <davis@rstudio.com>
2019-09-12 10:13:53 -0400

    Fix logic in `on_event()`

--- R/on-event.R
+++ R/on-event.R
@@ -1,53 +1,67 @@
+# `on_event()` is mainly useful for constructing a schedule where the actual
+# event needs to be adjusted if another event happens. i.e. "trash day" is
+# normally on Wednesday, but if Wednesday is a holiday then it falls on
+# Thursday instead.
+
+# This should not be confused with `sched_adjust()`, which takes an existing
+# set of dates and adjusts them to not fall on any `events` located by the
+# schedule.
+
+# `shift` here has to be a lubridate period. We need to be able to look
+# backwards using the shift to detect if the original date was at the
+# intersection of `x` and `event`. In that case, `shift` was applied to
+# move the date forward.
+
 #' @export
 on_event <- function(x, event, shift) {
-  if (is.character(shift)) {
-    shift <- period(shift)
-  }
-
-  if (is_formula(shift, scoped = TRUE, lhs = FALSE)) {
-    shift <- as_function(shift)
-  }
-
-  if (!is_function(shift) && !is.period(shift)) {
-    abort("`shift` must be a period or a function.")
-  }
-
+  shift <- check_period_shift(shift)
   assert_event(x, "`x`")
   assert_event(event)
 
   test <- function(env) {
     out <- event_is_impl(x, env)
 
-    problems <- out & event_is_impl(event, env)
+    not_currently_an_event <- event_is_impl(!event, env)
 
-    if (sum(problems) == 0L) {
+    # Does the current date lie on an `x` event, but not an `event` event?
+    out <- out & not_currently_an_event
+
+    is_event_requiring_shift <- x & event
+
+    dates <- current_date(env)
+    dates_unshifted <- dates - shift
+
+    env_unshifted <- init_context(dates_unshifted)
+
+    # Did the date before the shift lie on an `x` event, and an `event` event?
+    unshifted_dates_required_shift <- event_is_impl(is_event_requiring_shift, env_unshifted)
+    shifted_date_is_event <- not_currently_an_event & unshifted_dates_required_shift
+
+    if (sum(shifted_date_is_event) == 0L) {
       return(out)
     }
 
-    out[problems] <- FALSE
-
-    dates <- current_date(env)
-    problem_dates <- dates[problems]
-
-    if (is_function(shift)) {
-      adjusted <- shift(problem_dates)
-    } else {
-      adjusted <- problem_dates + shift
-    }
-
-    solutions <- vec_match(adjusted, dates)
-
-    solutions <- solutions[!is.na(solutions)]
-
-    out[solutions] <- TRUE
+    out[shifted_date_is_event] <- TRUE
 
     out
   }
 
   new_composite_event(
-    description = "Rolling event",
+    description = "Shifted event",
     test = test,
     events = list(x),
-    class = "rolling_event"
+    class = "shifted_event"
   )
+}
+
+check_period_shift <- function(shift) {
+  if (is.character(shift)) {
+    shift <- period(shift)
+  }
+
+  if (!is.period(shift)) {
+    abort("`shift` must be a period.")
+  }
+
+  shift
 }
