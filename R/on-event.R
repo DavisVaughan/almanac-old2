50933d624a4a8613febcb74063d8dcb0d72f0b07
DavisVaughan <davis@rstudio.com>
2019-09-13 16:28:32 -0400

    `on_event` docs

--- R/on-event.R
+++ R/on-event.R
@@ -1,17 +1,51 @@
-# `on_event()` is mainly useful for constructing a schedule where the actual
-# event needs to be adjusted if another event happens. i.e. "trash day" is
-# normally on Wednesday, but if Wednesday is a holiday then it falls on
-# Thursday instead.
+# TODO - What happens if you have the case of:
+# - Friday is normally trash day
+# - Unless Friday is a holiday
+# - In which case you'd move to the next non-event date
+# - And you want to define weekends as events too, so you'd roll to monday
+# So if you use `event_in()` on that monday, it should return TRUE
 
-# This should not be confused with `sched_adjust()`, which takes an existing
-# set of dates and adjusts them to not fall on any `events` located by the
-# schedule.
-
-# `jump` here has to be a lubridate period. We need to be able to look
-# backwards using the jump to detect if the original date was at the
-# intersection of `x` and `event`. In that case, `jump` was applied to
-# move the date forward.
-
+#' Events related to other events
+#'
+#' `on_event()` defines an alternative date to use as an event if `event`
+#' occurs. It is usually useful as a way to deviate from a normal schedule in
+#' the case of an unusual circumstance.
+#'
+#' @param x `[event]`
+#'
+#'    An event defining normal behavior.
+#'
+#' @param event `[event]`
+#'
+#'    An event defining when a `jump` should occur.
+#'
+#' @param jump `[Period(1) / integer(1)]`
+#'
+#'    A jump defining the deviation from normal behavior.
+#'
+#' @examples
+#' # Imagine the garbage truck normally comes every Monday, but if
+#' # Monday is a state holiday, then it comes on Tuesday instead. In this case
+#' # we can use `event = on_state_holiday` as a trigger to define an alternative
+#' # event rule.
+#'
+#' on_normal_trash_day <- on_wday("Monday")
+#' on_state_holiday <- on_month("Sep") & on_mweek(1) & on_wday("Monday")
+#'
+#' on_trash_day <- on_event(on_normal_trash_day, on_state_holiday, days(1))
+#'
+#' # A Monday in September
+#' event_in("2019-09-09", on_normal_trash_day)
+#' event_in("2019-09-09", on_trash_day)
+#'
+#' # Labor Day Monday should not be trash day
+#' event_in("2019-09-02", on_normal_trash_day)
+#' event_in("2019-09-02", on_trash_day)
+#'
+#' # The day after Labor Day Monday is trash day
+#' event_in("2019-09-03", on_normal_trash_day)
+#' event_in("2019-09-03", on_trash_day)
+#'
 #' @export
 on_event <- function(x, event, jump) {
   jump <- check_jump(jump)
