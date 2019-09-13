c755c097fa273ce1106a9c59c0646ee92b5224aa
DavisVaughan <davis@rstudio.com>
2019-09-13 16:08:39 -0400

    `sch_seq` docs

--- R/sch-seq.R
+++ R/sch-seq.R
@@ -1,6 +1,22 @@
+#' Generate events in a schedule
+#'
+#' `sch_seq()` generates all events in a schedule from `from` to `to`,
+#' inclusive.
+#'
+#' @param from,to `[Date(1)]`
+#'
+#'    Dates defining the range to look for events in the `schedule`.
+#'
+#' @param schedule `[schedule / event]`
+#'
+#'    A schedule or event.
+#'
+#' @examples
+#' sch_seq("2019-01-01", "2019-01-31", on_mday(12) | on_wday("Mon"))
+#'
 #' @export
 sch_seq <- function(from, to, schedule) {
-  assert_schedule(schedule)
+  schedule <- as_schedule(schedule)
 
   from <- vec_cast_date(from, x_arg = "from")
   to <- vec_cast_date(to, x_arg = "to")
