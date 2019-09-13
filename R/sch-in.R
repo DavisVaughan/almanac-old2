50fb380c35669f46e71229cdb95450b90d75da77
DavisVaughan <davis@rstudio.com>
2019-09-13 16:05:12 -0400

    `sch_in` docs

--- R/sch-in.R
+++ R/sch-in.R
@@ -1,3 +1,20 @@
+#' Is `x` in the schedule?
+#'
+#' `sch_in()` checks if `x` is in the set of dates defined by the schedule.
+#'
+#' @details
+#'
+#' `sch_in()` takes the union of the events in the schedule to construct a
+#' single event on which to use [event_in()].
+#'
+#' @param x `[Date]`
+#'
+#'    A vector of dates.
+#'
+#' @param schedule `[schedule]`
+#'
+#'    A schedule.
+#'
 #' @export
 sch_in <- function(x, schedule) {
   x <- vec_cast_date(x)
