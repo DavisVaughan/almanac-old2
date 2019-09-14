83bb0cdc745311fe1a9a20cfa5c82bec742c4920
DavisVaughan <davis@rstudio.com>
2019-09-13 22:14:26 -0400

    Add `with_limit()` for maximum number of occurrences

--- R/on-count.R
+++ R/on-count.R
@@ -1,3 +1,9 @@
+# Solves - The twelfth occurance of the event in the quarter
+# on_count(on_wday(c("Tues", "Thurs"), 12, "quarter"))
+
+# TODO - Maybe allow within to pass through unchanged to floor_date, as it
+# can also take "2 months". So the above example could be within "2 quarters"
+
 on_count <- function(event, n, within = "month") {
   assert_event(event)
 
@@ -40,5 +46,55 @@ on_count <- function(event, n, within = "month") {
   new_event(
     description = "On count",
     test = test
+  )
+}
+
+# Solves the problem of "5 occurrences since `since`"
+# For example: Jan 24th, for 5 occurrences since 2010-01-01.
+# Days before 2010-01-01 are automatically considered non-events.
+
+with_limit <- function(event, n, since) {
+
+  test <- function(env) {
+    x <- current_date(env)
+
+    until <- max(x)
+
+    if (since > until) {
+      return(rep(FALSE, vec_size(x)))
+    }
+
+    range <- seq(since, until, by = "1 day")
+
+    range_loc <- event_in(range, event)
+
+    if (length(range_loc) == 0L) {
+      return(rep(FALSE, vec_size(x)))
+    }
+
+    range_pos <- which(range_loc)
+    n_events_since <- length(range_pos)
+
+    n <- min(n_events_since, n)
+
+    range_pos_clamped <- range_pos[seq2(1L, n)]
+
+    event_dates <- range[range_pos_clamped]
+
+    out <- vec_init(logical(), n = vec_size(x))
+
+    before_event <- x < since
+    after_event <- !before_event
+
+    out[before_event] <- FALSE
+    out[after_event] <- vec_in(x[after_event], event_dates)
+
+    out
+  }
+
+  new_composite_event(
+    description = glue("With occurrence limit: {n}"),
+    test = test,
+    events = list(event)
   )
 }
